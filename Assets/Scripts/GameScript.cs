fad60014cd070d1693d3f61784eda17f3939d495
Adrien <napalm_ground@hotmail.com>
2020-04-02 10:02:27 +0200

    Shuffle card list

--- Assets/Scripts/GameScript.cs
+++ Assets/Scripts/GameScript.cs
@@ -24,38 +24,33 @@ public class GameScript : MonoBehaviour
 
     private void Start()
     {
+        DeckInit();
+    }
 
-        deckPlayer_1.Add(GetCardFromCollection(0));
-        deckPlayer_1.Add(GetCardFromCollection(0));
-        deckPlayer_1.Add(GetCardFromCollection(1));
-        deckPlayer_1.Add(GetCardFromCollection(2));
-        deckPlayer_1.Add(GetCardFromCollection(1));
-        deckPlayer_1.Add(GetCardFromCollection(1));
-        deckPlayer_1.Add(GetCardFromCollection(0));
-        deckPlayer_1.Add(GetCardFromCollection(0));
-        deckPlayer_1.Add(GetCardFromCollection(1));
-        deckPlayer_1.Add(GetCardFromCollection(2));
-        deckPlayer_1.Add(GetCardFromCollection(1));
-        deckPlayer_1.Add(GetCardFromCollection(1));
-        deckPlayer_1.Add(GetCardFromCollection(0));
-        deckPlayer_1.Add(GetCardFromCollection(0));
-        deckPlayer_1.Add(GetCardFromCollection(1));
-        deckPlayer_1.Add(GetCardFromCollection(2));
-        deckPlayer_1.Add(GetCardFromCollection(1));
-        deckPlayer_1.Add(GetCardFromCollection(1));
-        deckPlayer_1.Add(GetCardFromCollection(0));
-        deckPlayer_1.Add(GetCardFromCollection(0));
-        deckPlayer_1.Add(GetCardFromCollection(1));
-        deckPlayer_1.Add(GetCardFromCollection(2));
-        deckPlayer_1.Add(GetCardFromCollection(1));
-        deckPlayer_1.Add(GetCardFromCollection(1));
-
-        DisplayCardList(deckPlayer_1);
+    private void Update()
+    {
+        if (Input.GetKeyDown("space"))
+        {
+            ClearCardList();
+            ShuffleCardList(deckPlayer_1);
+            DisplayCardList(deckPlayer_1);
+        }
     }
 
     public Card GetCardFromCollection(int arg_cardIndex)
     {
         return cardCollection.GetComponent<CollectionScript>().GetCardCollection()[arg_cardIndex];
+    }
+
+    public List<Card> DeckInit ()
+    {
+        List<Card> deck = new List<Card>();
+
+        deckPlayer_1.Add(GetCardFromCollection(0));
+        deckPlayer_1.Add(GetCardFromCollection(1));
+        deckPlayer_1.Add(GetCardFromCollection(2));
+
+        return deck;
     }
 
     public void DisplayCardList(List<Card> arg_cardList)
@@ -65,6 +60,25 @@ public class GameScript : MonoBehaviour
             GameObject loc_instCard = Instantiate(CardSelectPrefab) as GameObject;
             loc_instCard.GetComponent<CardSelectPrefabScript>().SetCard(lp_card);
             loc_instCard.transform.SetParent(cardListDisplay.transform, false);
+        }
+    }
+
+    public void ClearCardList()
+    {
+        foreach (Transform child in cardListDisplay.transform)
+        {
+            GameObject.Destroy(child.gameObject);
+        }
+    }
+
+    public void ShuffleCardList(List<Card> arg_cardList)
+    {
+        for (int i = 0; i < arg_cardList.Count; i++)
+        {
+            Card temp = arg_cardList[i];
+            int randomIndex = Random.Range(i, arg_cardList.Count);
+            arg_cardList[i] = arg_cardList[randomIndex];
+            arg_cardList[randomIndex] = temp;
         }
     }
 }
